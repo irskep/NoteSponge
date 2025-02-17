4514b9492af9ddd1e03d2d5b28667fe3c0795da9
Steve Landey <steve@steveasleep.com>
2025-02-17 14:21:36 -0800

    Add missing menu items

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -50,6 +50,8 @@ pub fn create_app_menu<R: Runtime>(app: &tauri::App<R>) -> tauri::menu::Menu<R> 
         .separator()
         .item(&view_all_pages)
         .item(&search)
+        .separator()
+        .close_window()
         .build()
         .expect("failed to create file submenu");
 
@@ -65,9 +67,14 @@ pub fn create_app_menu<R: Runtime>(app: &tauri::App<R>) -> tauri::menu::Menu<R> 
         .build()
         .expect("failed to create edit submenu");
 
+    let window_submenu = SubmenuBuilder::new(app, "Window")
+        .minimize()
+        .build()
+        .expect("failed to create window submenu");
+
     // Build the complete menu
     MenuBuilder::new(app)
-        .items(&[&app_submenu, &file_submenu, &edit_submenu])
+        .items(&[&app_submenu, &file_submenu, &edit_submenu, &window_submenu])
         .build()
         .expect("failed to create menu")
 }
