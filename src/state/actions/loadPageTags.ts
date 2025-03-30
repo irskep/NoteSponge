d3d8a02abc87b00d9d0f1fea7950b0dd1e572513
Steve Landey <steve@steveasleep.com>
2025-03-29 17:27:23 -0700

    Fix bug in page tag loading

--- src/state/actions/loadPageTags.ts
+++ src/state/actions/loadPageTags.ts
@@ -1,10 +1,12 @@
 import { getPageTags } from "@/services/db/actions/tags";
 import { getDefaultStore } from "jotai";
-import { pageTagAtoms } from "../pageState";
+import { pageIdAtom, pageTagAtoms } from "../pageState";
 
 export async function loadPageTags(pageId: number) {
   const store = getDefaultStore();
   const tags = await getPageTags(pageId);
   store.set(pageTagAtoms.tags, (prev) => ({ ...prev, [pageId]: tags }));
-  store.set(pageTagAtoms.activeTagsWrittenToDatabase, tags);
+  if (pageId === store.get(pageIdAtom)) {
+    store.set(pageTagAtoms.activeTagsWrittenToDatabase, tags);
+  }
 }
